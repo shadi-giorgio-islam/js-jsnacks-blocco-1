acb3645ee537a84131f4569d56d9bc5ad7d3af1f
Giorgio <shadigiorgioislam@gmail.com>
2020-12-18 10:32:39 +0100

    esercizio 1 fine

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// ESERCIZIO 1
+// var numero;
+// var somma = 0;
+// var i = 0;
+
+// esercizio con for
+
+// for (i = 0; i < 5; i++) {
+//   numero = parseInt(prompt('Inserisci numero'));
+//   somma += numero;
+// }
+//
+// console.log(somma);
+
+// esercizo con while
+
+// while (i < 5) {
+//   i++;
+//   numero = parseInt(prompt('Inserisci numero'));
+//   somma += numero;
+// }
+//
+// console.log(somma);
